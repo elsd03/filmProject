3f94ec9677a3878165ffaad5a4af7ed83934bcae
Elshad <147345963+elsd03@users.noreply.github.com>
2024-05-15 12:51:50 +0400

    Add files via upload
    npm install
    npm start

---
+++ src/pages/main/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Movies } from '../../components/movies';
+import { Header } from '../../components/navbar';
+import './style.css';
+
+const MainPage = () => {
+
+    return (
+        <div className="main-page">
+            <Header />
+            <main className="main-page__content">
+                <section className="main-page__main-section">
+                    <div className="main-page__movies">
+                        <Movies />
+                    </div>
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default MainPage;
